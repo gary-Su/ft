117fd9007822566720d450ca9005a77ac2789efa
Zihua Su <234278195@qq.com>
2018-08-28 10:10:15 +0800

    initialize ft project

---
+++ src/main/java/com/website/ft/FtApplication.java
@@ -0,0 +1,12 @@
+package com.website.ft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtApplication.class, args);
+	}
+}
