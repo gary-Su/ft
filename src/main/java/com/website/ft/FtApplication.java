51a5f6a089027aec88a36e701ff6824d717cadc9
Zihua Su <suzihua@wondersgroup.com>
2018-08-28 15:42:05 +0800

    1

--- src/main/java/com/website/ft/FtApplication.java
+++ src/main/java/com/website/ft/FtApplication.java
@@ -2,8 +2,9 @@ package com.website.ft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class FtApplication {
 
 	public static void main(String[] args) {
